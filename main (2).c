86d3e3e9724e8441fcb8e43245bba03391609285
BALAJI <70253892+BALAJIRAGUNATHAN@users.noreply.github.com>
2022-01-05 17:24:58 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int n = 4;
+    int adjacency_matrix[][4] = {
+        {0, 1, 1, 0},   
+        {0, 0, 0, 1}, 
+        {0, 0, 0, 1},   
+        {0, 0, 0, 0}  
+    };
+    int indegrees[n];
+    bool visited[n];
+    memset(&indegrees, 0, sizeof(*indegrees) * n);
+    memset(&visited, false, sizeof(*visited) * n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            indegrees[i] += adjacency_matrix[j][i];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!indegrees[j] && !visited[j]) {
+                visited[j] = true;
+                printf("%d ", j + 1);
+
+                for (int k = 0; k < n; k++) {
+                    if (adjacency_matrix[j][k]) {
+                        adjacency_matrix[j][k] = 0;
+                        indegrees[k]--;
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
